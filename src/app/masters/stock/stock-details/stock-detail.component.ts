77e1f1c69dc9e81bd83bf669117d2e6163e6ec44
rajesh <rajesh@tatrasdata.com>
2018-08-14 17:13:37 +0530

    added stock item detail

--- src/app/masters/stock/stock-details/stock-detail.component.ts
+++ src/app/masters/stock/stock-details/stock-detail.component.ts
@@ -5,6 +5,10 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {FormBuilder, Validators} from "@angular/forms";
 import {StockService} from "../services/stock.service";
 import {StockDetailResponse} from "../models/stock-detail.model";
+import {LedgerResponse} from "../../ledger/models/ledger.model";
+import {LedgerService} from "../../ledger/services/ledger.service";
+import {UnitService} from "../../unit/services/unit.service";
+import {UnitResponse} from "../../unit/models/unit.model";
 
 @Component({
   selector: 'app-stock-detail',
@@ -16,15 +20,23 @@ export class StockDetailComponent implements OnInit, OnDestroy {
 
   public stockItemId;
   public stockItemSubscription: Subscription;
-  constructor( private stockService: StockService,private _route: ActivatedRoute, private _formBuilder: FormBuilder,private router : Router) { }
+  public ledgerDataSubscription: Subscription;
+  public unitDataSubscription: Subscription;
+
+  constructor( private stockService: StockService,private ledgerService: LedgerService, private unitService: UnitService, private _route: ActivatedRoute, private _formBuilder: FormBuilder,private router : Router) { }
   public stock;
   public sForm;
+  public ledgerData;
+  public unitData;
   ngOnInit() {
-    //this.ledgerForm();
+    this.stockForm();
     this._route.params.subscribe((params) => {
       this.stockItemId = params.id;
      // this.getLedgerData();
     });
+
+      this.getLedgerData();
+      this.getUnitData();
 
 
   }
@@ -75,41 +87,109 @@ export class StockDetailComponent implements OnInit, OnDestroy {
 
   }
 
-  // ledgerForm(){
-  //   this.lForm = this._formBuilder.group({
-  //     ledgerId: [0],
-  //     ledgerName: ['', [Validators.required, Validators.minLength(4)]],
-  //     ledgerGroupID: ['', Validators.required],
-  //     rateofTax: ['', Validators.required],
-  //     calculatedOn: ['', Validators.required],
-  //     taxType: ['', Validators.required],
-  //     company_ID: ['', Validators.required],
-  //     uSerID: ['', Validators.required]
-  //
-  //   });
-  // }
-  //
-  //
-  // get ledgerName() { return this.lForm.get('ledgerName'); }
-  //
-  // get ledgerGroupID() { return this.lForm.get('ledgerGroupID'); }
-  //
-  // get rateofTax() { return this.lForm.get('rateofTax'); }
-  //
-  //
-  // get calculatedOn() { return this.lForm.get('calculatedOn'); }
-  //
-  //
-  // get taxType() { return this.lForm.get('taxType'); }
-  //
-  // get company_ID() { return this.lForm.get('company_ID'); }
-  //
-  // get uSerID() { return this.lForm.get('uSerID'); }
-  //
-  //
-  //
+  stockForm(){
+    this.sForm = this._formBuilder.group({
+      stockItemID: [0],
+      itemName: ['', [Validators.required, Validators.minLength(4)]],
+      itemCode: ['', Validators.required],
+      stockGroup: ['', Validators.required],
+      unit: ['', Validators.required],
+      minimum_Order_Level: ['', Validators.required],
+      minimum_Order_Qty: ['', Validators.required],
+      bufferQty: ['', Validators.required],
+      hsnCode: ['', Validators.required],
+      taxrate: ['', Validators.required],
+      barcodeapplicable: ['', Validators.required],
+      barcodelength: ['', Validators.required],
+      branchName: ['', Validators.required],
+      activeStatus: ['', Validators.required],
+      company_ID: ['', Validators.required],
+        userID:   ['', Validators.required],
+        stockItemOpeningBal:  this._formBuilder.group({
+            stockItemOPID: [''],
+            stockitem_ID: [''],
+            location_ID: [''],
+            locationName: [''],
+            qty: [''],
+            rate: [''],
+            amount: [''],
+            modifiedDateTime: ['']
+        }),
+
+        stockItemSuppliers:  this._formBuilder.group({
+            stokItemSupplierID: [''],
+            stockitem_ID: [''],
+            ledger: [''],
+            orderPercentage: [''],
+            leadTime: [''],
+            modifiedDateTime: ['']
+        }),
+
+
+
+
+    });
+  }
+
+
+  get itemName() { return this.sForm.get('itemName'); }
+
+  get itemCode() { return this.sForm.get('itemCode'); }
+
+  get stockGroup() { return this.sForm.get('stockGroup'); }
+
+
+  get unit() { return this.sForm.get('unit'); }
+
+
+  get minimum_Order_Level() { return this.sForm.get('minimum_Order_Level'); }
+
+  get minimum_Order_Qty() { return this.sForm.get('minimum_Order_Qty'); }
+
+  get bufferQty() { return this.sForm.get('bufferQty'); }
+
+    get hsnCode() { return this.sForm.get('hsnCode'); }
+
+    get taxrate() { return this.sForm.get('taxrate'); }
+
+    get barcodeapplicable() { return this.sForm.get('barcodeapplicable'); }
+
+
+    get barcodelength() { return this.sForm.get('barcodelength'); }
+
+
+    get branchName() { return this.sForm.get('branchName'); }
+
+    get activeStatus() { return this.sForm.get('activeStatus'); }
+
+    get company_ID() { return this.sForm.get('company_ID'); }
+
+    get userID() { return this.sForm.get('userID'); }
+
+    get stockItemOPID() { return this.sForm.get('stockItemOpeningBal').get('stockItemOPID'); }
+
+    getLedgerData() {
+        this.ledgerDataSubscription = this.ledgerService.getAllLedgers().subscribe((res: LedgerResponse) => {
+            this.ledgerData = res.data;
+
+        });
+    }
+
+
+    getUnitData() {
+        this.unitDataSubscription = this.unitService.getAllUnits().subscribe((res: UnitResponse) => {
+            this.unitData = res.data;
+
+        });
+
+
+    }
+
+
   ngOnDestroy() {
     this.stockItemSubscription.unsubscribe();
+      this.ledgerDataSubscription.unsubscribe();
+      this.unitDataSubscription.unsubscribe();
   }
   //
   //
